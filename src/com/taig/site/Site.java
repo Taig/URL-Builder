562fdcc8217fdc84bf382bd59b40255ea5369c24
Taig <taig@AiR>
2012-12-07 00:07:46 +0100

    Improved code layout, fixed spelling errors and added missing documentation.

--- src/com/taig/site/Site.java
+++ src/com/taig/site/Site.java
@@ -11,92 +11,99 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 /**
- * A Site is a programmatic representation of a Website, managing its basic meta
- * information to provide simple access.
+ * A Site is a programmatic representation of a Website, managing its basic meta information to provide simple HTTP
+ * access.
  */
 public class Site
 {
+	/**
+	 * All possible Site-Schemes. Currently only <code>HTTP</code> and <code>HTTPS</code> are supported.
+	 */
 	public enum Scheme
 	{
 		HTTP( "http" ), HTTPS( "https" );
 
-		private String	name;
+		/**
+		 * The actual protocol name as it's used in the URL.
+		 */
+		private String protocol;
 
-		private Scheme( String name )
+		private Scheme( String protocol )
 		{
-			this.name = name;
+			this.protocol = protocol;
 		}
 
-		@Override
-		public String toString()
+		/**
+		 * Get the Scheme's protocol name as it is used in the URL.
+		 *
+		 * @return The Schemes protocol name (e.g. "http").
+		 */
+		public String getProtocol()
 		{
-			return name;
+			return protocol;
 		}
 	}
 
 	/**
 	 * The charset that is used for encoding.
 	 */
-	private String				charset		= "UTF-8";
+	private String charset = "utf-8";
 
 	/**
 	 * The Site's URL-scheme (e.g. "http" or "https").
 	 */
-	private String				scheme;
+	private String scheme;
 
 	/**
 	 * The Site's URL-username.
 	 */
-	private String				username;
+	private String username;
 
 	/**
 	 * The Site's URL-password.
 	 */
-	private String				password;
+	private String password;
 
 	/**
 	 * The Site's URL-subdomains (e.g. "www").
 	 */
-	private List<String>		subdomains	= new ArrayList<String>( 3 );
+	private List<String> subdomains = new ArrayList<String>( 3 );
 
 	/**
 	 * The Site's URL-host (e.g. "localhost" or "example.org").
 	 */
-	private String				host;
+	private String host;
 
 	/**
 	 * The Site's URL-port (e.g. "80").
 	 */
-	private int					port		= 80;
+	private int port = 80;
 
 	/**
 	 * The Site's URL-paths (e.g. "home" or ["user", "taig"]).
 	 */
-	private List<String>		paths		= new ArrayList<String>( 3 );
+	private List<String> paths = new ArrayList<String>( 3 );
 
 	/**
 	 * The Site's URL-file (e.g. "home.html").
 	 */
-	private String				file;
+	private String file;
 
 	/**
 	 * The Site's URL-parameters (e.g. "<id, 3>" or "<session, ASDF>").
 	 */
-	private Map<String, String>	parameters	= new LinkedHashMap<String, String>( 3 );
+	private Map<String, String> parameters = new LinkedHashMap<String, String>( 3 );
 
 	/**
 	 * The Site's URL-fragment (e.g. "http://example.org#fragment").
 	 */
-	private String				fragement;
+	private String fragment;
 
 	/**
-	 * Construct a Site with a minimum of information (e.g.
-	 * "http://example.org").
-	 * 
-	 * @param scheme
-	 *            The Site's URL-scheme (e.g. "http" or "https").
-	 * @param host
-	 *            The Site's URL-host (e.g. "localhost" or "example").
+	 * Construct a Site with a minimum of information (e.g. "http://example.org").
+	 *
+	 * @param scheme The Site's URL-scheme (e.g. "http" or "https").
+	 * @param host   The Site's URL-host (e.g. "localhost" or "example").
 	 */
 	public Site( Scheme scheme, String host )
 	{
@@ -106,29 +113,31 @@ public class Site
 
 	/**
 	 * Construct a Site from an {@link URL}.
-	 * 
-	 * @param url
+	 *
+	 * @param url A valid {@link URL} object.
 	 */
 	public Site( URL url )
 	{
 		setScheme( url.getProtocol() );
 
 		String[] hosts = url.getHost().split( "." );
+
 		int i;
+
 		for( i = 0; i < hosts.length - 2; i++ )
 		{
 			addSubdomain( hosts[i] );
 		}
 
-		String[] authentification = new String[2];
-		authentification = url.getUserInfo().split( ":", 2 );
-		setAuthentification( authentification[0], authentification[1] );
+		String[] authentication = url.getUserInfo().split( ":", 2 );
+		setAuthentication( authentication[0], authentication[1] );
 
 		setHost( hosts[i] + hosts[i + 1] );
 
 		setPort( url.getPort() > 0 ? url.getPort() : url.getDefaultPort() );
 
 		String[] paths = url.getPath().split( "/" );
+
 		for( i = 0; i < paths.length - 1; i++ )
 		{
 			addPath( paths[i] );
@@ -144,10 +153,10 @@ public class Site
 		}
 
 		String[] parameters = url.getQuery().split( "&" );
-		String[] parameter = new String[2];
+
 		for( i = 0; i < parameters.length; i++ )
 		{
-			parameter = parameters[i].split( "=", 2 );
+			String[] parameter = parameters[i].split( "=", 2 );
 			putParameter( parameter[0], parameter[1] );
 		}
 
@@ -156,14 +165,14 @@ public class Site
 
 	/**
 	 * Construct a copy of the given {@link Site}.
-	 * 
-	 * @param site
+	 *
+	 * @param site A Site object that is used as template for the new Site.
 	 */
 	public Site( Site site )
 	{
 		setCharset( site.getCharset() );
 		setScheme( site.getScheme() );
-		setAuthentification( site.getUsername(), site.getPassword() );
+		setAuthentication( site.getUsername(), site.getPassword() );
 		getSubdomains().addAll( site.getSubdomains() );
 		setHost( site.getHost() );
 		setPort( site.getPort() );
@@ -175,8 +184,8 @@ public class Site
 
 	/**
 	 * Retrieve the charset that is used for encoding.
-	 * 
-	 * @return
+	 *
+	 * @return The Site's charset.
 	 */
 	public String getCharset()
 	{
@@ -185,9 +194,8 @@ public class Site
 
 	/**
 	 * Set the charset that is used for encoding.
-	 * 
-	 * @param charset
-	 *            The charset that is used for encoding.
+	 *
+	 * @param charset The charset that is used for encoding.
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site setCharset( String charset )
@@ -198,7 +206,7 @@ public class Site
 
 	/**
 	 * Retrieve the Site's URL-scheme.
-	 * 
+	 *
 	 * @return The Site's URL-scheme (e.g. "http" or "https").
 	 */
 	public String getScheme()
@@ -208,9 +216,20 @@ public class Site
 
 	/**
 	 * Set the Site's URL-scheme.
-	 * 
-	 * @param scheme
-	 *            An URL-scheme (e.g. "http" or "https").
+	 *
+	 * @param scheme An URL-{@link Scheme} (e.g. "http" or "https").
+	 * @return Current instance of {@link Site} to allow method chaining.
+	 */
+	public Site setScheme( Scheme scheme )
+	{
+		this.scheme = scheme.getProtocol();
+		return this;
+	}
+
+	/**
+	 * Set the Site's URL-scheme.
+	 *
+	 * @param scheme An URL-scheme (e.g. "http" or "https").
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site setScheme( String scheme )
@@ -220,21 +239,8 @@ public class Site
 	}
 
 	/**
-	 * Set the Site's URL-scheme.
-	 * 
-	 * @param scheme
-	 *            An URL-{@link Scheme} (e.g. "http" or "https").
-	 * @return Current instance of {@link Site} to allow method chaining.
-	 */
-	public Site setScheme( Scheme scheme )
-	{
-		this.scheme = scheme.toString();
-		return this;
-	}
-
-	/**
 	 * Retrieve the Site's URL-username.
-	 * 
+	 *
 	 * @return The Site's URL-username (as from "http://user:pass@example.org").
 	 */
 	public String getUsername()
@@ -244,7 +250,7 @@ public class Site
 
 	/**
 	 * Retrieve the Site's URL-password.
-	 * 
+	 *
 	 * @return The Site's URL-password (as from "http://user:pass@example.org").
 	 */
 	public String getPassword()
@@ -253,17 +259,13 @@ public class Site
 	}
 
 	/**
-	 * Set the Site's URL-authentification.
-	 * 
-	 * @param username
-	 *            The Site's URL-username (as from
-	 *            "http://user:pass@example.org").
-	 * @param password
-	 *            The Site's URL-password (as from
-	 *            "http://user:pass@example.org").
+	 * Set the Site's URL-authentication.
+	 *
+	 * @param username The Site's URL-username (as from "http://user:pass@example.org").
+	 * @param password The Site's URL-password (as from "http://user:pass@example.org").
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
-	public Site setAuthentification( String username, String password )
+	public Site setAuthentication( String username, String password )
 	{
 		this.username = username;
 		this.password = password;
@@ -271,8 +273,8 @@ public class Site
 	}
 
 	/**
-	 * Retrieve the Site's URL-subdomains.
-	 * 
+	 * Retrieve the Site's URL-subdomains
+	 *
 	 * @return The Site's URL-subdomains (e.g. "www").
 	 */
 	public List<String> getSubdomains()
@@ -282,9 +284,8 @@ public class Site
 
 	/**
 	 * Set the Site's URL-subdomains.
-	 * 
-	 * @param subdomains
-	 *            URL-subdomains (e.g. "www").
+	 *
+	 * @param subdomains URL-subdomains (e.g. "www").
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site setSubdomains( List<String> subdomains )
@@ -295,9 +296,8 @@ public class Site
 
 	/**
 	 * Add a subdomain to the Site's URL.
-	 * 
-	 * @param subdomain
-	 *            An URL-subdomain (e.g. "www").
+	 *
+	 * @param subdomain An URL-subdomain (e.g. "www").
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site addSubdomain( String subdomain )
@@ -308,7 +308,7 @@ public class Site
 
 	/**
 	 * Retrieve the Site's URL-host.
-	 * 
+	 *
 	 * @return The Site's URL-host (e.g. "localhost" or "example.org").
 	 */
 	public String getHost()
@@ -318,9 +318,8 @@ public class Site
 
 	/**
 	 * Set the Site's URL-scheme.
-	 * 
-	 * @param scheme
-	 *            An URL-host (e.g. "localhost" or "example.org").
+	 *
+	 * @param host An URL-host (e.g. "localhost" or "example.org").
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site setHost( String host )
@@ -331,7 +330,7 @@ public class Site
 
 	/**
 	 * Retrieve the Site's URL-port.
-	 * 
+	 *
 	 * @return The Site's URL-port (e.g. "80").
 	 */
 	public int getPort()
@@ -341,9 +340,8 @@ public class Site
 
 	/**
 	 * Set the Site's URL-port.
-	 * 
-	 * @param port
-	 *            An URL-port (e.g. "80");
+	 *
+	 * @param port An URL-port (e.g. "80");
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site setPort( int port )
@@ -354,7 +352,7 @@ public class Site
 
 	/**
 	 * Retrieve the Site's URL-paths.
-	 * 
+	 *
 	 * @return The Site's URL-paths (e.g. "home" or ["user", "taig"]).
 	 */
 	public List<String> getPaths()
@@ -364,9 +362,8 @@ public class Site
 
 	/**
 	 * Set the Site's URL-paths.
-	 * 
-	 * @param paths
-	 *            URL-paths (e.g. "home" or ["user", "taig"]).
+	 *
+	 * @param paths URL-paths (e.g. "home" or ["user", "taig"]).
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site setPaths( List<String> paths )
@@ -377,9 +374,8 @@ public class Site
 
 	/**
 	 * Add a path to the Site's URL.
-	 * 
-	 * @param path
-	 *            An URL-path (e.g. "home" or ["user", "taig"]).
+	 *
+	 * @param path An URL-path (e.g. "home" or ["user", "taig"]).
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site addPath( String path )
@@ -390,8 +386,8 @@ public class Site
 
 	/**
 	 * Retrieve the Site's URL-file (e.g. "home.html").
-	 * 
-	 * @return
+	 *
+	 * @return The Site's file.
 	 */
 	public String getFile()
 	{
@@ -399,10 +395,10 @@ public class Site
 	}
 
 	/**
-	 * Set the Site's URL-file (e.g. "home.html").
-	 * 
-	 * @param file
-	 * @return
+	 * Set the Site's URL-file .
+	 *
+	 * @param file URL-file (e.g. "home.html").
+	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site setFile( String file )
 	{
@@ -412,7 +408,7 @@ public class Site
 
 	/**
 	 * Retrieve the Site's URL-parameters.
-	 * 
+	 *
 	 * @return The Site's URL-parameters (e.g. "<id, 3>" or "<session, ASDF>").
 	 */
 	public Map<String, String> getParameters()
@@ -421,22 +417,9 @@ public class Site
 	}
 
 	/**
-	 * Retrieve the Site's URL-parameter for the given key.
-	 * 
-	 * @param key
-	 *            An URL-parameter.
-	 * @return
-	 */
-	public String getParameter( String key )
-	{
-		return parameters.get( key );
-	}
-
-	/**
 	 * Set the Site's URL-parameters.
-	 * 
-	 * @param params
-	 *            URL-parameters (e.g. "<id, 3>" or "<session, ASDF>").
+	 *
+	 * @param params URL-parameters (e.g. "<id, 3>" or "<session, ASDF>").
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site setParameters( Map<String, String> params )
@@ -446,13 +429,22 @@ public class Site
 	}
 
 	/**
-	 * Add a parameter (<key, value>) to the Site's URL (e.g. "<id, 3>" or
-	 * "<session, ASDF>"). If the key already exists it will be overridden.
-	 * 
-	 * @param key
-	 *            The param's key.
-	 * @param value
-	 *            The param's value.
+	 * Retrieve the Site's URL-parameter for the given key.
+	 *
+	 * @param key An URL-parameter.
+	 * @return The parameter value that is associated with the given key or <code>null</code> if none exists.
+	 */
+	public String getParameter( String key )
+	{
+		return parameters.get( key );
+	}
+
+	/**
+	 * Add a parameter (<key, value>) to the Site's URL (e.g. "<id, 3>" or "<session, ASDF>"). If the key already exists it
+	 * will be overridden.
+	 *
+	 * @param key   The parameter's key.
+	 * @param value The parameter's value.
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site putParameter( String key, String value )
@@ -463,32 +455,30 @@ public class Site
 
 	/**
 	 * Retrieve the Site's URL-fragment.
-	 * 
+	 *
 	 * @return The Site's URL-fragment (e.g. "http://example.org#fragment").
 	 */
 	public String getFragment()
 	{
-		return fragement;
+		return fragment;
 	}
 
 	/**
 	 * Set the Site's URL-fragment.
-	 * 
-	 * @param fragment
-	 *            An URL-fragment (e.g. "http://example.org#fragment").
+	 *
+	 * @param fragment An URL-fragment (e.g. "http://example.org#fragment").
 	 * @return Current instance of {@link Site} to allow method chaining.
 	 */
 	public Site setFragment( String fragment )
 	{
-		this.fragement = fragment;
+		this.fragment = fragment;
 		return this;
 	}
 
 	/**
 	 * Retrieve the Site's complete URL.
-	 * 
-	 * @return A fully qualified URL (e.g.
-	 *         "http://www.example.org/home?user=taig").
+	 *
+	 * @return A fully qualified URL (e.g. "http://www.example.org/home?user=taig").
 	 */
 	public URL getUrl() throws MalformedURLException
 	{
@@ -497,9 +487,9 @@ public class Site
 
 	/**
 	 * {@link URLEncoder#encode(String)} a String.
-	 * 
-	 * @param value
-	 * @return
+	 *
+	 * @param value The String that shall be URL-encoded.
+	 * @return An URL-encoded String based on the {@link #charset} or the input value if the encoding fails.
 	 */
 	private String encode( String value )
 	{
@@ -509,9 +499,21 @@ public class Site
 		}
 		catch( UnsupportedEncodingException exception )
 		{
-			exception.printStackTrace();
 			return value;
 		}
+	}
+
+	@Override
+	public boolean equals( Object object )
+	{
+		if( object != null && object instanceof Site )
+		{
+			Site site = (Site) object;
+
+			return site.toString().equals( this.toString() );
+		}
+
+		return false;
 	}
 
 	@Override
@@ -572,18 +574,5 @@ public class Site
 		}
 
 		return urlBuilder.toString();
-	}
-
-	@Override
-	public boolean equals( Object object )
-	{
-		if( object != null && object instanceof Site )
-		{
-			Site site = (Site) object;
-
-			return site.toString().equals( this.toString() );
-		}
-
-		return false;
 	}
 }
